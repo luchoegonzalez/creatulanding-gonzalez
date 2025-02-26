252d09b23bf0ea0fa501594c5d9acaa2f9a94632
luchoegonzalez <legonzalez@dc.uba.ar>
2025-02-26 02:47:58 -0300

    creacion de NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import magdaLogo from '../assets/logo.jpg';
+
+function NavBar() {
+
+  return (
+    <>
+      <nav className="font-[Nunito] text-lg text-black bg-pink-200">
+        <div className="flex justify-between items-center w-9/10 m-auto pt-2 pb-2">
+          <a href="#" className="">
+            <img src={magdaLogo} alt="logo magda3d" className='w-20 rounded-full'/>
+          </a>
+          <ul className='flex gap-1.5'>
+            <li><a className='hover:bg-pink-600 hover:text-white duration-150 p-2 rounded-xl' href="#">Figuras</a></li>
+            <li><a className='hover:bg-pink-600 hover:text-white duration-150 p-2 rounded-xl' href="#">Llaveros</a></li>
+            <li><a className='hover:bg-pink-600 hover:text-white duration-150 p-2 rounded-xl' href="#">Mates</a></li>
+            <li><a className='hover:bg-pink-600 hover:text-white duration-150 p-2 rounded-xl' href="#">Macetas</a></li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  )
+}
+  
+export default NavBar
