286804efd4bfaa50c41dfc52a0571c3a0eee5086
luchoegonzalez <legonzalez@dc.uba.ar>
2025-03-27 04:56:29 -0300

    update limit de productos al inicio

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -7,7 +7,7 @@ function ItemListContainer() {
   const {id} = useParams();
 
   useEffect(() => {
-    const urlProducts = id ? `https://api.escuelajs.co/api/v1/products?categoryId=${id}` : 'https://api.escuelajs.co/api/v1/products';
+    const urlProducts = id ? `https://api.escuelajs.co/api/v1/products?categoryId=${id}` : 'https://api.escuelajs.co/api/v1/products?offset=0&limit=50';
     fetch(urlProducts)
     .then(response => response.json())
     .then(data => setItems(data));
