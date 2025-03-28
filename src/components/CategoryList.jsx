971596cb797449fde7710a50cce6dd2c7408811f
luchoegonzalez <legonzalez@dc.uba.ar>
2025-03-27 21:36:23 -0300

    add active category

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,19 +1,30 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export default function CategoryList({ categories, toggleMenu }) {
+  const location = useLocation();
+  
   return (
     <ul className="flex flex-col md:flex-row items-center gap-4 md:gap-1">
-      {categories.map(category => (
-        <li key={category}>
-          <Link onClick={toggleMenu} className='p-3 hover:bg-pink-600 hover:text-white duration-150 rounded-xl' to={`/category/${category}`}>
-            {capitalizeFirstLetter(category)}
-          </Link>
-        </li>
-      ))}
+      {categories.map((category) => {
+        const isActive = location.pathname.includes(`/category/${encodeURIComponent(category)}`);
+
+        return (
+          <li key={category}>
+            <Link
+              onClick={toggleMenu}
+              to={`/category/${category}`}
+              className={`block px-4 py-2 rounded-md transition-colors duration-200 
+                ${isActive ? "bg-pink-500 text-white" : "hover:bg-pink-300"}`}
+            >
+              {capitalizeFirstLetter(category)}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
