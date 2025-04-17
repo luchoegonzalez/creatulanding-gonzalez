011584eb624bc9f34ba965fd4a06508b9fdeb35f
luchoegonzalez <legonzalez@dc.uba.ar>
2025-04-17 05:01:41 -0300

    acomodo mejor imagenes en itemGallery

--- src/components/ItemGallery.jsx
+++ src/components/ItemGallery.jsx
@@ -5,10 +5,18 @@ export default function ImageGallery({ images }) {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <div className="flex flex-col md:flex-row items-center md:items-start gap-4 w-full">
-      {/* Miniaturas */}
+    <div className="flex flex-col md:flex-row-reverse items-center md:items-start md:justify-center gap-4 w-full">
+
+      <div className="w-80 h-[400px] border border-pink-200 rounded-2xl shadow-sm overflow-hidden">
+        <img
+          src={images[activeIndex]}
+          alt={`Producto ${activeIndex + 1}`}
+          className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
+        />
+      </div>
+
       {images.length > 1 && (
-        <div className="flex md:flex-col gap-3 md:mr-4">
+        <div className="flex flex-row md:flex-col gap-3 mt-4 md:mt-0">
           {images.map((img, index) => (
             <button
               key={index}
@@ -17,7 +25,7 @@ export default function ImageGallery({ images }) {
                 index === activeIndex
                   ? "border-pink-500"
                   : "border-pink-200"
-              } overflow-hidden transition hover:scale-105`}
+              } overflow-hidden transition hover:scale-105 hover:cursor-pointer`}
             >
               <img
                 src={img}
@@ -28,15 +36,6 @@ export default function ImageGallery({ images }) {
           ))}
         </div>
       )}
-
-      {/* Imagen Principal */}
-      <div className="w-80 h-[400px] border border-pink-200 rounded-2xl shadow-sm overflow-hidden">
-        <img
-          src={images[activeIndex]}
-          alt={`Producto ${activeIndex + 1}`}
-          className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
-        />
-      </div>
     </div>
   );
 }
